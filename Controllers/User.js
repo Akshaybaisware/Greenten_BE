00b3238cbda67132c1095f43a2c60bb66eacb21e
akshaybaisware <akshaybaisware1998@gmail.com>
2024-09-23 12:14:19 +0530

    updated backend

--- Controllers/User.js
+++ Controllers/User.js
@@ -71,8 +71,8 @@ const add_user = async(req, res) => {
 
             status: "Registered",
             password,
-            totalAssignmentLimit: 540,
-            pendingAssignmentCount: 540,
+            totalAssignmentLimit: 400,
+            pendingAssignmentCount: 400,
         });
 
         const savedUser = await newUser.save();
